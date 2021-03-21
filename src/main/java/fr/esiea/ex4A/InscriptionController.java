4d40f1c6f1e62c23b6613482796f2dccc94b7bef
Celine Lam <lam@et.esiea.fr>
2021-03-21 22:28:40 +0100

    Add APIs HTTP JSON

--- src/main/java/fr/esiea/ex4A/InscriptionController.java
+++ src/main/java/fr/esiea/ex4A/InscriptionController.java
@@ -1,2 +1,30 @@
-package fr.esiea.ex4A;public class InscriptionController {
+package fr.esiea.ex4A;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class InscriptionController {
+
+    @PostMapping(value = "api/inscription", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void inscription(@RequestBody UserInfo userinfo) {
+        System.out.println(userinfo.userEmail);
+        System.out.println(userinfo.userName);
+        System.out.println((userinfo.userTweeter));
+        System.out.println(userinfo.userCountry);
+        System.out.println(userinfo.userSex);
+        System.out.println(userinfo.userSexPref);
+    }
+    @GetMapping(value = "api/matches", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Match> getMatches(String name, String country) {
+        return List.of(
+            new Match("machin", "machin45"),
+            new Match("truc", "trucbidule")
+        );
+    }
 }
--- src/main/java/fr/esiea/ex4A/UserInfo.java
+++ src/main/java/fr/esiea/ex4A/UserInfo.java
@@ -1,2 +1,28 @@
-package fr.esiea.ex4A;public class UserInfo {
-}
+package fr.esiea.ex4A;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserInfo {
+    public final String userEmail;
+    public final String userName;
+    public final String userTweeter;
+    public final String userCountry;
+    public final String userSex;
+    public final String userSexPref;
+
+
+    public UserInfo(@JsonProperty("userEmail") String userEmail,
+                    @JsonProperty("userName") String userName,
+                    @JsonProperty("userTweeter") String userTweeter,
+                    @JsonProperty("userCountry") String userCountry,
+                   @JsonProperty("userSex") String userSex,
+                    @JsonProperty("userSexPref") String userSexPref) {
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.userTweeter = userTweeter;
+        this.userCountry = userCountry;
+        this.userSex = userSex;
+        this.userSexPref = userSexPref;
+    }
+    }
+
